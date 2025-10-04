e205af119f91bb2473a1caa6ac13cd4334df6263
Niamh Hogan <hniamhh1995@gmail.com>
2025-10-04 17:28:19 +0100

    created bankhol JSON file, read in json file

--- assignment02-bankholidays.py
+++ assignment02-bankholidays.py
@@ -6,4 +6,29 @@
 # bank holidays that are unique to northern Ireland 
 # compared to the rest of the UK (you can choose if you want to use the name or the date of the holiday to decide if it is unique.)
 
-# read in file
+# import JSON
+import json 
+
+# retrieve data from url
+
+# read in data from file
+# https://www.geeksforgeeks.org/python/read-json-file-using-python/
+with open('bankholidays.json', 'r') as file:
+    data = json.load(file)
+
+print(json.dumps(data, indent=4))
+
+# print northern ireland BH dates
+
+
+
+
+
+
+
+
+
+
+
+
+
