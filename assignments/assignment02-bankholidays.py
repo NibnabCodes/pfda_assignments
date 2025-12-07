0c6548374810d5d5d391d6b4cbe863a94495f565
NiamhHogan <hniamhh1995@gmail.com>
2025-12-07 13:09:23 +0000

    completed assign2-added explanations & references; formatted to PEP8

--- assignments/assignment02-bankholidays.py
+++ assignments/assignment02-bankholidays.py
@@ -1,65 +1,84 @@
+# assignment02-bankholidays.py
+# This program prints the dates of the bank holidays that occur
+# in Northern Ireland. It is then modified to print the bank
+# holiday names that are unique to Northern Ireland compared to the
+# rest of the UK.
 # Author: Niamh Hogan
-# This program prints out the dates of the 
-# bank holidays that occur in northern Ireland.
 
-# The program is then modified to print the 
-# bank holidays (names) that are unique to northern Ireland 
-# compared to the rest of the UK 
+import json
 
-# import JSON
-import json 
-
-# retrieve data from url
-
-# read in data from file
-# https://www.geeksforgeeks.org/python/read-json-file-using-python/
-
-with open('../data/bankholidays.json', 'r') as file:
+# Read JSON data from file
+with open("../data/bankholidays.json", "r") as file:
     data = json.load(file)
 
-# print file to check that it has been read in
-#print(json.dumps(data, indent=4))
+# Print file contents to verify it has been read correctly
+print(json.dumps(data, indent=4))
 
-# access nested dict keys
-# to get Northern Ireland holidays
-# https://labex.io/tutorials/python-how-to-access-nested-keys-in-a-python-json-object-395034
+# Access nested dictionary keys to get Northern Ireland holidays
 ni_events = data["northern-ireland"]["events"]
 
-# Print just the dates using for-loop
-# https://www.w3schools.com/python/python_for_loops.asp
+# Print only the dates of Northern Ireland bank holidays
 print("Bank Holiday dates in Northern Ireland:")
 for ev in ni_events:
     print(ev["date"])
 
-
-# modify program to print out unique ni bank
-# holiday names
-
-# get events for other regions
-# note: already extracted ni events above
+# Get events for other UK regions
 ew_events = data["england-and-wales"]["events"]
 scot_events = data["scotland"]["events"]
 
-# Collect all other holiday names
+# Collect holiday names from other regions
 other_titles = {ev["title"] for ev in ew_events + scot_events}
 
-# Collect NI holiday names to
-# prevent duplicates from being printed
+# Collect Northern Ireland holiday names
 ni_titles = {ev["title"] for ev in ni_events}
 
-# combine for loop and if statement
-# to print only NI holidays that are unique to NI
-#https://sparkbyexamples.com/python/python-for-loop-with-if-statement/
+# Print only Northern Ireland holidays that do not appear elsewhere
 print("Unique Northern Ireland bank holidays:")
 for title in ni_titles:
     if title not in other_titles:
         print(title)
 
-
-
-
-
-
-
-
-
+# ---------------------------------------------------------------
+# EXPLANATIONS & REFERENCES
+#
+# Reading JSON:
+# I used the json module to open and load the JSON file.
+# Reference:
+# https://www.geeksforgeeks.org/python/read-json-file-using-python/
+#
+# Printing JSON contents:
+# I used json.dumps() with the indent parameter to print the JSON
+# data in a readable, structured format. This allowed me to visually
+# check that the file contents loaded correctly.
+# Reference:
+# https://www.w3schools.com/python/ref_json_dumps.asp
+#
+# Accessing nested keys:
+# I extracted data from nested dictionaries using standard key access.
+# Reference:
+# https://labex.io/tutorials/python-how-to-access-nested-keys-in-a-python-json-object-395034
+#
+# Using a for-loop to print values:
+# I used a simple for-loop to print each bank holiday date.
+# Reference:
+# https://www.w3schools.com/python/python_for_loops.asp
+#
+## Extracting and preparing holiday names:
+# I accessed the "events" lists for England and Wales, and for Scotland,
+# by selecting their keys from the main JSON dictionary. I then used set
+# comprehension to collect the holiday names from these regions into a
+# single set called other_titles. I also used set comprehension to collect
+# all Northern Ireland holiday names into ni_titles so that I could compare
+# them without duplicates.
+# References:
+# https://labex.io/tutorials/python-how-to-access-nested-keys-in-a-python-json-object-395034
+# https://www.w3schools.com/python/python_sets_comprehension.asp
+#
+# Filtering unique values:
+# I used a for-loop with an if-statement to compare each Northern
+# Ireland holiday name against the holiday names from other UK
+# regions. If the title was not found in the other set, I printed it.
+# References:
+# https://sparkbyexamples.com/python/python-for-loop-with-if-statement/
+# https://docs.python.org/3/reference/expressions.html#membership-test-details
+# ---------------------------------------------------------------
